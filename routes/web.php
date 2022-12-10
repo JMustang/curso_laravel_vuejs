7c5968d5a38123e045eea707fa49d0f6ef86215c
Junior <eecfredes@gmail.com>
2022-12-10 19:49:40 -0300

    :sparkles: feat: rotas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'HOME';
+});
+
+Route::get('/aboutus', function () {
+    return 'ABOUT US';
+});
+
+Route::get('/contact', function () {
+    return 'CONTACT';
 });
