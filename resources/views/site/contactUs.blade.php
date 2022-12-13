0bb560ff120fe20cae44c01187875b55c9a9d23c
Junior <eecfredes@gmail.com>
2022-12-13 19:03:13 -0300

    :sparkles: feat: menu de navegacao

--- resources/views/site/contactUs.blade.php
+++ resources/views/site/contactUs.blade.php
@@ -1,1 +1,13 @@
 <h3>CONTACT US {view}</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+    <a href="/aboutus">Sobre nos</a>
+    </li>
+    <li>
+    <a href="/contactus">Contatos</a>
+    </li>
+</ul>
