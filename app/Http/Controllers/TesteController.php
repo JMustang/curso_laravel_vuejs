480426f8cc5064fa1ae636cfb3979cf173aadcf7
Junior <eecfredes@gmail.com>
2022-12-14 21:13:36 -0300

    :sparkles: feat: Encaminhando parametros de rota para o view

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $p1, int $p2) {
-        echo 'A soma de $p1 + p2 e: '.($p1+$p2);
+        // echo 'A soma de $p1 + p2 e: '.($p1+$p2);
+        // return view('site.teste', ['p1' => $p1, 'p2' => $p2]);array associativo
+
+        // return view('site.teste', compact('p1', 'p2')); compact
+
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2); //with()
     }
 }
