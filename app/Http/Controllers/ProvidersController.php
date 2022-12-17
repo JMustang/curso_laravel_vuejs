9959d32a00505b44e846d79c82266b5224f68abf
Junior <eecfredes@gmail.com>
2022-12-16 22:04:47 -0300

    :sparkles: feat: @unless

--- app/Http/Controllers/ProvidersController.php
+++ app/Http/Controllers/ProvidersController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class ProvidersController extends Controller
 {
     public function index(){
-        $providers = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'];
+        $providers = [
+            0 => ['name' => 'Fornecedor 1', 'status' => 'N']
+        ];
 
         return view('app.providers.index', compact('providers'));
     }
--- resources/views/app/providers/index.blade.php
+++ resources/views/app/providers/index.blade.php
@@ -12,10 +12,25 @@
     // }
 @endphp
 
-@if(count($providers) > 0 && count($providers) < 10)
+{{-- @if(count($providers) > 0 && count($providers) < 10)
         <h3>Fornecedores cadastrados</h3>
 @elseif(count($providers) > 10)
         <h3>Varios fornecedores cadastrados</h3>
 @else
         <h3>Ainda nao existe fornecedores cadastrados</h3>
+@endif --}}
+
+
+
+Fornecedor: {{ $providers[0]['name'] }}
+<br>
+Status: {{ $providers[0]['status'] }}
+<br>
+@if(!($providers[0]['status']=='S'))
+        Fornecedor inativo
 @endif
+<br>
+@unless($providers[0]['status']=='S')
+        Fornecedor inativo
+@endunless
+<br>
